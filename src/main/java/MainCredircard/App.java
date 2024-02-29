836b2348cc574f641f10ddce640d07cc9e83577a
lohit <soaadmn@gmail.com>
2024-02-29 08:33:29 +0530

    Business cards

---
+++ src/main/java/MainCredircard/App.java
@@ -0,0 +1,13 @@
+package MainCredircard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi ALL!" );
+    }
+}
